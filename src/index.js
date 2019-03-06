f226d43627829a0adfa04eea27d1f09683ab6e9f
Swaagie <info@martijnswaagman.nl>
2019-03-06 14:42:20 +0100

    [fix] cleaner table

--- src/index.js
+++ src/index.js
@@ -4,15 +4,51 @@ const { Command, flags } = require('@oclif/command');
 const { Reader, Parser } = require('@smartmeter/core');
 const { cli } = require('cli-ux');
 
+const kw = 'kw/h';
+const hr = {
+  lowTariffReceived: {
+    title: 'Power consumption (low)',
+    unit: kw
+  },
+  highTariffReceived: {
+    title: 'Power consumption (high)',
+    unit: kw
+  },
+  lowTariffDelivered: {
+    title: 'Power production (low)',
+    unit: kw
+  },
+  highTariffDelivered: {
+    title: 'Power production (high)',
+    unit: kw
+  },
+  consumption: {
+    title: 'Current consumption',
+    unit: kw
+  },
+  production: {
+    title: 'Current production',
+    unit: kw
+  },
+  gasConsumption: {
+    title: 'Gas consumption',
+    unit: 'm3'
+  }
+}
+
 class SmartmeterCliCommand extends Command {
   tabular(time, data) {
-    cli.table(Object.keys(data).map(key => ({ value: data[key], key })), {
+    cli.table(Object.keys(data).map(key => ({ key: hr[key][title], value: data[key], unit: hr[key][unit] })), {
       key: {
         header: 'Measurement',
-        minWidth: 25
+        minWidth: 40
       },
-      value : {
+      value: {
         header: 'Value'
+        minWidth: 10
+      },
+      unit: {
+        header: ''
       }
     });
 
